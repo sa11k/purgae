9c97dab60be0c25a1b52a5ba2c068d41fbd96cc9
sa11k <sdk4590@gmail.com>
2022-09-05 14:36:23 +0900

    dd

--- Front/src/vite-env.d.ts
+++ Front/src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_METAMASK_ADDRESS: string;
+  readonly VITE_METAMASK_SECRET_KEY: string;
+  readonly VITE_PORT: number;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
