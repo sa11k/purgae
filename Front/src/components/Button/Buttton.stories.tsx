f30fad299ceae87d18e5ea5d4fcb8aa99dbf886a
sa11k <sdk4590@gmail.com>
2022-09-06 11:13:59 +0900

    #53 Style.브랜치 생성 커밋-PDB

--- Front/src/components/Button/Buttton.stories.tsx
+++ Front/src/components/Button/Buttton.stories.tsx
@@ -14,7 +14,7 @@ export const Secondary = () => <Button variant='secondary'>Secondary</Button>
 export const Success = () => <Button variant='success'>Success</Button>
 export const Danger = () => <Button variant='danger'>Danger</Button>
 
-// args는 prop을 위한 임시변수
+// args는 prop을 위한 임시변수.
 
 // const Template = (args: any) => <Button {...args} />
 
