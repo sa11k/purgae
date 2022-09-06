4cbd9cb82b56065cd8e24c3b9d59ebe71e78d05c
sa11k <sdk4590@gmail.com>
2022-09-06 10:51:42 +0900

    #53 Feat.스토리북 설정 및 파일구조 초기설정

---
+++ Front/src/components/Button/Buttton.stories.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Button from "./Button";
+
+export default {
+  title: 'Form/Button',
+  component: Button,
+  args: {
+    children: 'Button'
+  }
+}
+
+export const Primary = () => <Button variant='primary'>Primary</Button>
+export const Secondary = () => <Button variant='secondary'>Secondary</Button>
+export const Success = () => <Button variant='success'>Success</Button>
+export const Danger = () => <Button variant='danger'>Danger</Button>
+
+// args는 prop을 위한 임시변수
+
+// const Template = (args: any) => <Button {...args} />
+
+// export const PrimaryA = Template.bind({})
+// PrimaryA.args = {
+//   variant: 'primary',
+//   // children: 'Primary Args',
+// }
+
+// export const SecondaryA = Template.bind({})
+// SecondaryA.args = {
+//   variant: 'secondary',
+//   // children: 'Secondary Args',
+// }
+
+// export const LongPrimaryA = Template.bind({})
+// LongPrimaryA.args = {
+//   ...PrimaryA.args,
+//   // children: 'Long Primary Args',
+// }
---
+++ Front/src/components/Subscription/Subscription.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Primary } from '../Button/Buttton.stories'
+import { Large } from '../Input/Input.stories'
+
+
+export default {
+  title: 'Form/Subscription',
+}
+
+export const PrimarySubscription = () => (
+  <>
+    <Large />
+    <Primary />
+  </>
+)
