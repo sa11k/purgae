76208a5fc0e33718d64ee2eba069ef5b79cf260c
sa11k <sdk4590@gmail.com>
2022-09-07 17:22:17 +0900

    #68 Feat. 메타마스크 연결 로직 구현 추후 버튼 및 web3로 변경

--- Front/src/app/App.tsx
+++ Front/src/app/App.tsx
@@ -1,12 +1,42 @@
-import { PrimarySubscription } from "@/components/Subscription/Subscription.stories";
+import Home from "@/components/home/Home";
+import { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
 const App = () => {
+  //todo. 실행시, 크립토좀비 참고
+  // - metamask검사
+  const [account, setAccount] = useState ("");
+
+  const getAccount = async () => {
+    try {
+      if (window.ethereum) {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        // -> window.ethereum.request 찍어보기
+
+        setAccount(accounts[0]);
+      } else {
+        alert("Install Metamask!");
+      }
+    } catch (error) {
+      console.error(error); 
+    }
+  };
+
+  useEffect(() => {
+    getAccount(); 
+  }, []);
+
+
 
   return (
-    <div className="App">
-      <PrimarySubscription />
-    </div>
+    <>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+    </Routes>
+    </>
   );
 }
 
---
+++ Front/src/components/auth/login/Login.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Web3 from "web3";
+
+type Props = {}
+
+const Login = (props: Props) => {
+
+  return (
+    <div>
+      <button></button>
+
+    </div>
+  )
+}
+
+export default Login
