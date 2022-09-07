1775a28e34d3f437367f5cbf9a509c530361228f
sa11k <sdk4590@gmail.com>
2022-09-07 09:49:04 +0900

    #49 Fix.conflict해결-PC/SDK

---
+++ Back/src/main/java/com/ssafy/purgae/database/entity/User.java
@@ -0,0 +1,34 @@
+package com.ssafy.purgae.database.entity;
+
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+
+@Table(name = "User")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User extends BaseEntity{
+
+    @Column(name = "walletAddress", length = 50, nullable = false)
+    String walletAddress;
+
+    @Column(name = "nickname", length = 16, nullable = false)
+    String nickname;
+
+    @Column(name = "profileImage", length = 50)
+    String profileImage;
+
+    @Column(name = "gameScore")
+    @ColumnDefault("0")
+    Long gameScore;
+
+    @Column(name = "profilePublic", nullable = false)
+    boolean profilePublic = true;
+
+
+}
