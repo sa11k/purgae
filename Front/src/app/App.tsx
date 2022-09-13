ab3500defa272f3a928a8e8310e79914b32214f4
sa11k <sdk4590@gmail.com>
2022-09-13 10:40:27 +0900

    #68 Fix.폴더구조 변경

--- Front/src/app/App.tsx
+++ Front/src/app/App.tsx
@@ -1,10 +1,10 @@
-import Login from "@/components/auth/login/Login";
-import Home from "@/components/home/Home";
+import Login from "@/features/auth/login/Login";
+import Home from "@/features/home/Home";
 import { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Web3 from "web3";
-import { useAppSelector } from "./hooks";
+import { useAppSelector } from "../hooks/storehooks";
 
 
 const ROPSTEN_URL = 'https://ropsten.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161';
@@ -12,7 +12,7 @@ const ROPSTEN_URL = 'https://ropsten.infura.io/v3/9aa3d95b3bc440fa88ea12eaa44561
 
 const App = () => {
   // https://redux-toolkit.js.org/tutorials/typescript
-  const account = useAppSelector(state => state.auth.userInfo.account)
+  // const account = useAppSelector(state => state.auth.userInfo.account)
   
   // todo. 실행시, 크립토좀비 참고
   // // - metamask검사
