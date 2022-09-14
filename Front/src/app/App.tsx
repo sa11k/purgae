e9260d7b9e3b0bcf39e04d14d1851ef79faf71f5
sa11k <sdk4590@gmail.com>
2022-09-14 14:29:02 +0900

    #186 Feat.Redux와 ReduxRTK 개발환경 설정-PJK

--- Front/src/app/App.tsx
+++ Front/src/app/App.tsx
@@ -1,36 +1,34 @@
 import Login from "@/features/auth/login/Login";
 import Home from "@/features/home/Home";
+import Counter from "@/features/counter/Counter";
 import { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import Web3 from "web3";
-import { useAppSelector } from "../hooks/storehooks";
+// import Web3 from "web3"
+import { useAppSelector } from "@/hooks/storeHooks";
 
-
-const ROPSTEN_URL = 'https://ropsten.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161';
-
+const ROPSTEN_URL =
+  "https://ropsten.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161";
 
 const App = () => {
   // https://redux-toolkit.js.org/tutorials/typescript
   // const account = useAppSelector(state => state.auth.userInfo.account)
-  
+
   // todo. 실행시, 크립토좀비 참고
   // // - metamask검사
 
   useEffect(() => {
-    // getAccount(); 
+    // getAccount();
   }, []);
-
-
 
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/login" element={<Login />}/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/counter" element={<Counter />} />
+      </Routes>
     </>
   );
-}
+};
 
 export default App;
