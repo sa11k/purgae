e930ec4c10405a62b48b9ceb999d76230d6694e9
sa11k <sdk4590@gmail.com>
2022-09-14 15:35:24 +0900

    #189 Feat. Styled Components 글로벌 스타일 설정

--- Front/src/app/styles.ts
+++ Front/src/app/styles.ts
@@ -1,13 +1,21 @@
-import { createGlobalStyle } from 'styled-components';
+// 글로벌 스타일 만들기 위한 createGlobalStyle 불러오기
+import { createGlobalStyle } from "styled-components";
 
+// 스타일 초기화를 위한 reset 불러오기
+import reset from "styled-reset";
+
+// 글로벌 스타일 만들기
 // 외부에서 import 해서 사용
-//app.tsx에 적용함
+// app.tsx에 적용함
 export const GlobalStyle = createGlobalStyle`
   /* 여기에 css파일 작성하면 됩니다. */
-  html, body, h1, h2, h3, h4, h5, h6, p, blockquote, code, img, dl, dt, dd, ol, ul, li, fieldset, legend, caption { margin: 0; padding: 0; border: 0; }
+  // reset 사용해 스타일 초기화
+${reset}
+html, body, h1, h2, h3, h4, h5, h6, p, blockquote, code, img, dl, dt, dd, ol, ul, li, fieldset, legend, caption { margin: 0; padding: 0; border: 0; }
 div, span, article, section, header, footer, p, ul, li, fieldset, legend, label, a, nav { box-sizing: border-box; }
 html {
     height: 100%;
+    font-family: "Pretendard Variable", -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
 }
 body {
     min-height: 100%;
