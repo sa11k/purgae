26c86d1e1395267a14eb987d437512fa125d1091
sa11k <sdk4590@gmail.com>
2022-09-16 21:58:25 +0900

    #195 Feat.input 컴포넌트 구현-PJK

--- Front/src/common/Input/Input.stories.tsx
+++ Front/src/common/Input/Input.stories.tsx
@@ -1,13 +1,31 @@
+import { Meta, Story } from "@storybook/react";
 import CommonInput from "./CommonInput";
+import { CommonInputProps } from "./CommonInput.types";
 
 export default {
   title: "Form/Input",
   component: CommonInput,
+} as Meta;
+
+export const Default: Story<CommonInputProps> = (args) => <CommonInput {...args}> 아이디</CommonInput>;
+export const Basic = () => (
+  <CommonInput id="input-default" placeHolder="아이디를 입력해주세요" fontSize="18px">
+    아이디
+  </CommonInput>
+);
+export const Error = () => (
+  <CommonInput id="input-error" status={false} errorMessage="에러가 발생했습니다" fontSize="18px">
+    아이디
+  </CommonInput>
+);
+
+export const Full = () => (
+  <CommonInput id="input-default" placeHolder="아이디를 입력해주세요" fontSize="18px" width="100%">
+    아이디
+  </CommonInput>
+);
+export const NoProps = () => <CommonInput id="input-noprops" />;
+
+Default.args = {
+  id: "input-default",
 };
-
-export const defaultInput = () => <CommonInput>라벨</CommonInput>;
-export const noLabelInput = () => <CommonInput />;
-
-// rename가능
-defaultInput.storyName = "default";
-noLabelInput.storyName = "noLabel";
