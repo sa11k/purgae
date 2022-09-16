c997ec90226be0e25e6d05c2cd24676c3774fe63
sa11k <sdk4590@gmail.com>
2022-09-17 02:59:09 +0900

    #198 Feat.ProfileImage 컴포넌트 개발-KJY

--- Front/src/features/counter/ThemeTest.tsx
+++ Front/src/features/counter/ThemeTest.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 // 꼭 이 파일에서 styled를 임포트 해주세요
 import { styled } from "../../styles/theme";
+import ProfileImage from "@/common/ProfileImage/ProfileImage";
 
 const Button = styled.button`
   border: none;
-  color: ${({ theme }) => theme.color.mainPrimary};
-  font-weight: ${({ theme }) => theme.fontWeight.bold};
+  color: ${({ theme }) => theme.colors.mainPrimary};
+  font-weight: ${({ theme }) => theme.fontWeights.bold};
   letter-spacing: ${({ theme }) => theme.letterSpacing.button};
 `;
 
 const ThemeTest = () => {
-  return <Button>ThemeTest</Button>;
+  return (
+    <div>
+      <Button>ThemeTest</Button>
+      <ProfileImage
+        size="extraLarge"
+        url="http://127.0.0.1:8080/ipfs/QmPSunAN8JYHYE811qYzTEtjoUDHivkFSsjijGDQ36JRRN/"
+      ></ProfileImage>
+    </div>
+  );
 };
 
 export default ThemeTest;
