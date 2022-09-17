19892c91b343f839804d3fd11fdd1dd0875d8c06
sa11k <sdk4590@gmail.com>
2022-09-18 00:06:25 +0900

    #197 Feat.버튼 컴포넌트 구현 완료-PJK

--- Front/src/common/Button/ButtonInput.styled.ts
+++ Front/src/common/Button/ButtonInput.styled.ts
@@ -26,7 +26,7 @@ export const OutLineButton = styled.button<{
   ${({ fontSize, theme }) => theme.mixins.font(fontSize, "700")};
   width: ${({ width }) => width};
   padding: 0.625rem 2.5rem;
-  border: ${({ theme, fontColor }) => `0.13rem solid ${theme.colors[fontColor]}`};
+  border: ${({ theme, fontColor }) => `0.125rem solid ${theme.colors[fontColor]}`};
   border-radius: 0.5rem;
   background-color: ${({ theme, bgColor }) => theme.colors[bgColor]};
 
