b5635b5d2f77ce1631dcd06bc61429dd10b33fb2
sa11k <sdk4590@gmail.com>
2022-09-18 00:00:10 +0900

    #197 Feat.버튼 컴포넌트 구현 완료-PJK

--- Front/src/common/Input/CommonInput.styled.ts
+++ Front/src/common/Input/CommonInput.styled.ts
@@ -4,7 +4,7 @@ import { styled } from "../../styles/theme";
 export const InputTag = styled.input<{ status: boolean }>`
   width: 100%;
   padding: 0.5rem 1rem;
-  border: ${({ status, theme }) => (status ? "0" : `1px solid ${theme.colors.red600}`)};
+  border: ${({ status, theme }) => (status ? "0" : `0.0625rem solid ${theme.colors.red600}`)};
   border-radius: 0.5rem;
   font-size: 0.9em;
   background-color: ${({ status, theme }) => (status ? theme.colors.white150 : theme.colors.red300p)};
@@ -29,7 +29,7 @@ export const ErrorMessage = styled.p`
 
 export const DivTag = styled.div<{ fontSize: string; width: string }>`
   ${({ theme }) => theme.mixins.flexBox("column", "flex-start", "center")};
-  ${(props) => props.theme.mixins.font(props.fontSize, "600")}
+  ${({ fontSize, theme }) => theme.mixins.font(fontSize, "600")};
   gap: 1rem;
   width: ${({ width }) => width};
   color: ${({ theme }) => theme.colors.mainParagraph};
