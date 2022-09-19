e910aeeef8b3f71ee20da75ab6eafca41089d4ec
sa11k <sdk4590@gmail.com>
2022-09-19 16:36:19 +0900

    #288 Feat.Card 컴포넌트 수정-KJY

--- Front/src/common/Card/Card.stories.tsx
+++ Front/src/common/Card/Card.stories.tsx
@@ -1,6 +1,6 @@
 import { Meta, Story } from "@storybook/react";
 import Card, { CardGroup } from "./Card";
-import { CardProps, CardGroupProps } from "./Card.types";
+import { CardGroupProps } from "./Card.types";
 
 export default {
   title: "Form/Card",
@@ -14,11 +14,7 @@ export default {
 const dummyUrl: string =
   "https://stickershop.line-scdn.net/stickershop/v1/product/2822/LINEStorePC/main.png;compress=true";
 
-// * 기본
-export const Default: Story<CardProps> = (args) => <Card {...args}></Card>;
-
-// * Card 여러개 보여줄 시
-export const Group: Story<CardGroupProps> = (args) => (
+export const Default: Story<CardGroupProps> = (args) => (
   <CardGroup {...args}>
     <Card url={dummyUrl} selected={true}></Card>
     <Card url={dummyUrl}></Card>
@@ -30,12 +26,3 @@ export const Group: Story<CardGroupProps> = (args) => (
     <Card url={dummyUrl}></Card>
   </CardGroup>
 );
-
-//* args의 기본 값
-Default.args = {
-  url: dummyUrl,
-};
-
-Group.args = {
-  width: "72rem",
-};
--- Front/src/common/Card/Card.styled.ts
+++ Front/src/common/Card/Card.styled.ts
@@ -2,9 +2,8 @@
 import { styled } from "../../styles/theme";
 
 export const Background = styled.div<{ selected: boolean }>`
-  height: 11.25rem;
-  width: 9.5rem;
-  margin: 1.25rem;
+  width: 100%;
+  aspect-ratio: 152/180;
   border: ${({ selected, theme }) => (selected ? `0.1876rem solid ${theme.colors.white}` : "0")};
   border-radius: 1.3125rem;
   background-color: ${({ theme }) => theme.colors.lightBlue300};
@@ -12,16 +11,23 @@ export const Background = styled.div<{ selected: boolean }>`
 `;
 
 export const Image = styled.div<{ url: string }>`
-  height: 9.25rem;
-  width: 9rem;
-  margin: auto auto;
+  width: 100%;
+  aspect-ratio: 152/148;
   background-image: url(${(props) => props.url});
   background-size: contain;
   background-repeat: no-repeat;
 `;
 
-export const Group = styled.div<{ width: string }>`
-  ${({ theme }) => theme.mixins.flexBox("row", "center", "start")};
-  flex-wrap: wrap;
-  width: ${(props) => props.width};
+export const Group = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(40%, auto));
+  row-gap: 6%;
+  column-gap: 2%;
+  width: 100%;
+  @media screen and (min-width: 768px) {
+    grid-template-columns: repeat(auto-fill, minmax(25%, auto));
+  }
+  @media screen and (min-width: 1350px) {
+    grid-template-columns: repeat(auto-fill, minmax(13%, auto));
+  }
 `;
