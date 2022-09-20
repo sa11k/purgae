543c96a927f7a9b23116da8fa1f0823c00be48d1
sa11k <sdk4590@gmail.com>
2022-09-20 22:30:58 +0900

    #291 Feat.수족관 3D CSS로 구현 및 마우스에 따라 움직이도록 개발-KJY

--- Front/src/common/Aquarium/Aquarium.styled.ts
+++ Front/src/common/Aquarium/Aquarium.styled.ts
@@ -1,53 +1,67 @@
 import { styled } from "@/styles/theme";
 
 export const Scene = styled.div`
-  position: relative;
-  width: 100%;
-  height: 100%;
-  perspective: 600px;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
 `;
 
-export const Container = styled.div`
+export const Cube = styled.div<{ rotationX: number; rotationY: number }>`
   position: relative;
+  transform-origin: 50%;
+  transform: ${(props) => `perspective(5000px) rotateY(${props.rotationX}deg) rotateX(${props.rotationY}deg)`};
+  transform-style: preserve-3d;
   width: 100%;
   height: 100%;
-  transform-style: preserve-3d;
 `;
 
 export const CubeFace = styled.div`
   position: absolute;
   width: 100%;
   height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const Front = styled(CubeFace)`
-  transform: rotateY(0deg) translateZ(300px);
-  border: 1px solid black;
+  z-index: 1;
+  transform: rotateY(0deg);
 `;
 
 export const Back = styled(CubeFace)`
-  transform: rotateY(180deg) translateZ(300px);
-  border: 1px solid black;
+  z-index: -1;
+  transform: rotateY(180deg) translateZ(100vw);
+  border: 2px solid white;
+  background-color: black;
 `;
 
 export const Right = styled(CubeFace)`
-  transform: rotateY(90deg) translateZ(300px);
-  border: 1px solid black;
+  z-index: 0;
+  width: 1000vw;
+  transform: rotateY(90deg) translateZ(-400vw);
+  background-color: black;
+  border: 1px solid white;
 `;
 
 export const Left = styled(CubeFace)`
-  transform: rotateY(-90deg) translateZ(300px);
-  position: absolute;
-  left: 0;
-  border: 1px solid black;
+  z-index: 0;
+  width: 1000vw;
+  transform: rotateY(90deg) translateZ(-500vw);
+  background-color: black;
+  border: 1px solid white;
 `;
 
 export const Top = styled(CubeFace)`
-  transform: rotateX(90deg) translateZ(300px);
-  border: 1px solid black;
+  z-index: 0;
+  transform: translateY(-50vh) rotateX(90deg) scaleY(5);
+  background-color: black;
+  border: 1px solid white;
 `;
 
 export const Bottom = styled(CubeFace)`
-  transform: rotateX(-90deg) translateZ(300px);
-  border: 1px solid black;
+  z-index: 0;
+  transform: translateY(50vh) rotateX(90deg) scaleY(5);
+  background-color: black;
+  border: 1px solid white;
 `;
--- Front/src/common/Aquarium/Aquarium.tsx
+++ Front/src/common/Aquarium/Aquarium.tsx
@@ -1,16 +1,26 @@
-import { Scene, Container, Front, Back, Right, Left, Top, Bottom } from "./Aquarium.styled";
+import { useState } from "react";
+import { Scene, Cube, Front, Back, Right, Left, Top, Bottom } from "./Aquarium.styled";
 
 const Aquarium = () => {
+  const [rotationX, setRotationX] = useState(0.0);
+  const [rotationY, setRotationY] = useState(0.0);
+  const handleMouseMove = (event: React.MouseEvent) => {
+    const x = (event.pageX / document.body.clientWidth) * 2 - 1;
+    const y = (event.pageY / document.body.clientHeight) * 2 - 1;
+    setRotationX(x * 3);
+    setRotationY(-y * 3);
+  };
+
   return (
     <Scene>
-      <Container>
+      <Cube onMouseMove={handleMouseMove} rotationX={rotationX} rotationY={rotationY}>
         <Front />
         <Back />
         <Right />
         <Left />
         <Top />
         <Bottom />
-      </Container>
+      </Cube>
     </Scene>
   );
 };
