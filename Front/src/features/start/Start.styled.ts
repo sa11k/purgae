aaa9eceeaab2a731d831661445da4da895e3683a
sa11k <sdk4590@gmail.com>
2022-09-20 23:24:33 +0900

    #291 Feat.시작 페이지에 home으로 가는 버튼 추가-KJY

---
+++ Front/src/features/start/Start.styled.ts
@@ -0,0 +1,18 @@
+import { styled } from "@/styles/theme";
+import { SolidButton } from "@/common/Button/Button.styled";
+
+export const Div = styled.div`
+  width: 100%;
+  height: 100%;
+  display: relative;
+`;
+
+export const Button = styled(SolidButton)`
+  display: absolute;
+  position: fixed;
+  left: 50%;
+  bottom: 12%;
+  z-index: 3;
+  transform: translate(-50%);
+  opacity: 0.7;
+`;
