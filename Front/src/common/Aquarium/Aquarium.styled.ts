fffb92eae315b73ac6cdfdb6dad4e09d47525a1e
sa11k <sdk4590@gmail.com>
2022-09-20 14:40:49 +0900

    #291 Feat.수족관 3D 틀 제작중-KJY

--- Front/src/common/Aquarium/Aquarium.styled.ts
+++ Front/src/common/Aquarium/Aquarium.styled.ts
@@ -1,3 +1,53 @@
 import { styled } from "@/styles/theme";
 
-export const;
+export const Scene = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  perspective: 600px;
+`;
+
+export const Container = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  transform-style: preserve-3d;
+`;
+
+export const CubeFace = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+`;
+
+export const Front = styled(CubeFace)`
+  transform: rotateY(0deg) translateZ(300px);
+  border: 1px solid black;
+`;
+
+export const Back = styled(CubeFace)`
+  transform: rotateY(180deg) translateZ(300px);
+  border: 1px solid black;
+`;
+
+export const Right = styled(CubeFace)`
+  transform: rotateY(90deg) translateZ(300px);
+  border: 1px solid black;
+`;
+
+export const Left = styled(CubeFace)`
+  transform: rotateY(-90deg) translateZ(300px);
+  position: absolute;
+  left: 0;
+  border: 1px solid black;
+`;
+
+export const Top = styled(CubeFace)`
+  transform: rotateX(90deg) translateZ(300px);
+  border: 1px solid black;
+`;
+
+export const Bottom = styled(CubeFace)`
+  transform: rotateX(-90deg) translateZ(300px);
+  border: 1px solid black;
+`;
--- Front/src/common/Aquarium/Aquarium.tsx
+++ Front/src/common/Aquarium/Aquarium.tsx
@@ -1,5 +1,18 @@
+import { Scene, Container, Front, Back, Right, Left, Top, Bottom } from "./Aquarium.styled";
+
 const Aquarium = () => {
-  return <div></div>;
+  return (
+    <Scene>
+      <Container>
+        <Front />
+        <Back />
+        <Right />
+        <Left />
+        <Top />
+        <Bottom />
+      </Container>
+    </Scene>
+  );
 };
 
 export default Aquarium;
