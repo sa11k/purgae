1c6f044b51c94e560dd87c9fe7694e34f4ef1d77
sa11k <sdk4590@gmail.com>
2022-09-23 16:28:29 +0900

    #290 Docs.vite-env, MetaEnv 파일 추가-SYJ

--- Front/src/utils/smart-contract/MetaEnv.ts
+++ Front/src/utils/smart-contract/MetaEnv.ts
@@ -15,4 +15,4 @@ export const TEST_WALLET_ADDRESS = `${import.meta.env.VITE_BE_METAMASK_ADDRESS}`
 export const TEST_WALLET_SECRET_KEY = `${import.meta.env.VITE_BE_METAMASK_SECRET_KEY}`;
 
 //* CONTRACT ADDRESS
-export const CONTRACT_ADDRESS = `${import.meta.env.VITE_BE_CONTRACT_ADDRESS}`;
+export const CONTRACT_ADDRESS = `${import.meta.env.VITE_CONTRACT_ADDRESS}`;
