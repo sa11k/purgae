844c58f1560d11373c0c275609cd208b2d8a3d70
sa11k <sdk4590@gmail.com>
2022-09-30 16:10:36 +0900

    #423 Feat.팔로우 팔로잉 모달 수정-KJY

--- Front/src/features/profile/components/FollowModal/FollowModal.tsx
+++ Front/src/features/profile/components/FollowModal/FollowModal.tsx
@@ -43,6 +43,20 @@ const FollowModal = ({
     setIsFollower(status);
   }, [status]);
 
+  // * 뒷배경 스크롤 방지
+  useEffect(() => {
+    document.body.style.cssText = `
+      position: fixed; 
+      top: -${window.scrollY}px;
+      overflow-y: scroll;
+      width: 100%;`;
+    return () => {
+      const scrollY = document.body.style.top;
+      document.body.style.cssText = "";
+      window.scrollTo(0, parseInt(scrollY || "0", 10) * -1);
+    };
+  }, []);
+
   return (
     <ModalContainer>
       {" "}
