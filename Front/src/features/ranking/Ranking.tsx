ee6bea54133ab46b424c34eeb6e2f5965eb68809
sa11k <sdk4590@gmail.com>
2022-10-01 21:39:34 +0900

    #457 Feat.랭킹 페이지 레이아웃 설계 및 디자인, API 제작중-SYJ

--- Front/src/features/ranking/Ranking.tsx
+++ Front/src/features/ranking/Ranking.tsx
@@ -1,15 +1,29 @@
-import { RootComponent } from "@/common/Common.styled";
-import { BgImg } from "./Ranking.styled";
+import {
+  RankingBackground,
+  RankingBackgroundImage,
+  RankingRootComponent,
+  RankingTitleWrapper,
+  RankingProomy,
+  RankingSpeechBubble,
+} from "./Ranking.styled";
+import RankingList from "./components/RankingList/RankingList";
 
 type Props = {};
 
 const Ranking = (props: Props) => {
   return (
     <>
-      <BgImg />
-      <RootComponent>
-        <div>asdf</div>
-      </RootComponent>
+      <RankingBackground>
+        <RankingBackgroundImage>
+          <RankingRootComponent>
+            <RankingTitleWrapper>
+              <RankingProomy src={"/assets/proomy/logo.png"} />
+              <RankingSpeechBubble>여러분의 기부 덕분에 해양 쓰레기가 줄고 있어요!</RankingSpeechBubble>
+            </RankingTitleWrapper>
+            <RankingList />
+          </RankingRootComponent>
+        </RankingBackgroundImage>
+      </RankingBackground>
     </>
   );
 };
