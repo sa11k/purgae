d8b40dc1c9984981cae2f1946551a37ff368f173
sa11k <sdk4590@gmail.com>
2022-10-03 23:40:15 +0900

    #454 Feat.랭킹 페이지 반응형 구현-SYJ

--- Front/src/features/ranking/components/RankingBar/RankingBar.tsx
+++ Front/src/features/ranking/components/RankingBar/RankingBar.tsx
@@ -16,11 +16,36 @@ const RankingBarBackground = styled.div`
   height: 3.7rem;
   border-radius: 0.3rem 0.3rem 0rem 0rem;
   box-shadow: ${({ theme }) => theme.shadows.shadow500};
+  @media screen and (max-width: 768px) {
+    width: 20rem;
+    grid-template-columns: 0.55fr 1.5fr 1fr;
+    padding: 0.5rem 1.5rem;
+    height: 3rem;
+  }
+`;
+
+const RankingSecondBarBackground = styled.div`
+  display: grid;
+  align-items: center;
+  grid-template-columns: 0.5fr 1.5fr 0.8fr;
+  width: 35rem;
+  padding: 0.5rem 2rem;
+  background-image: ${({ theme }) => theme.colors.gradient};
+  opacity: 0.5;
+  height: 3.7rem;
+  border-radius: 0.3rem 0.3rem 0rem 0rem;
+  box-shadow: ${({ theme }) => theme.shadows.shadow500};
+  @media screen and (max-width: 1350px) {
+    display: none;
+  }
 `;
 
 const RankingBarContent = styled.p`
   ${({ theme }) => theme.mixins.font("1rem", "500")};
   color: ${({ theme }) => theme.colors.white};
+  @media screen and (max-width: 768px) {
+    ${({ theme }) => theme.mixins.font("0.6rem", "500")};
+  }
 `;
 
 type Title = {
@@ -35,11 +60,11 @@ const RankingBar = (title: Title) => {
         <RankingBarContent>프로필</RankingBarContent>
         <RankingBarContent>{title.title}</RankingBarContent>
       </RankingBarBackground>
-      <RankingBarBackground>
+      <RankingSecondBarBackground>
         <RankingBarContent>순위</RankingBarContent>
         <RankingBarContent>프로필</RankingBarContent>
         <RankingBarContent>{title.title}</RankingBarContent>
-      </RankingBarBackground>
+      </RankingSecondBarBackground>
     </RankingBarWrapper>
   );
 };
--- Front/src/features/ranking/components/RankingList/RankingList.styled.ts
+++ Front/src/features/ranking/components/RankingList/RankingList.styled.ts
@@ -4,26 +4,45 @@ export const RankingTitleListWrapper = styled.div`
   ${({ theme }) => theme.mixins.flexBox("column", "center", "center")};
   padding: 0rem;
   gap: 1.5rem;
+  @media screen and (max-width: 768px) {
+    width: 20rem;
+  }
 `;
 
 export const RankingTitle = styled.div`
   ${({ theme }) => theme.mixins.flexBox("row", "center", "start")};
   ${({ theme }) => theme.mixins.font("1.8rem", "600")};
   color: ${({ theme }) => theme.colors.gray200};
-  width: 99%;
+  width: 100%;
   text-align: left;
   padding-bottom: 1rem;
+  @media screen and (max-width: 768px) {
+    ${({ theme }) => theme.mixins.font("1.2rem", "600")};
+    padding-bottom: 0rem;
+  }
 `;
 
 export const RankingListWrapper = styled.div`
   ${({ theme }) => theme.mixins.flexBox("column", "center", "center")};
   padding-bottom: 5rem;
+  @media screen and (max-width: 768px) {
+    width: 20rem;
+  }
 `;
 
 export const RankingContentWrapper = styled.div`
   display: grid;
+  justify-content: center;
+  align-items: center;
   grid-template-columns: 35rem 35rem;
   grid-template-rows: 1fr;
   column-gap: 4rem;
   padding-bottom: 6rem;
+  @media screen and (max-width: 1350px) {
+    grid-template-columns: 35rem;
+    grid-template-rows: 1fr;
+  }
+  @media screen and (max-width: 768px) {
+    grid-template-columns: 20rem;
+  }
 `;
