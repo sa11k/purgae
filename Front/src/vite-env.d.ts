6af935920c94079a0d53647f5fcc41181dca0df8
sa11k <sdk4590@gmail.com>
2022-10-04 22:20:17 +0900

    #566 Feat.트래킹 툴 설치-PJK

--- Front/src/vite-env.d.ts
+++ Front/src/vite-env.d.ts
@@ -7,6 +7,7 @@ interface ImportMetaEnv {
   readonly VITE_BE_METAMASK_SECRET_KEY: string;
   readonly VITE_PURGAE_NET_ALCHEMY: string;
   readonly VITE_KAKAO_APP_KEY: string;
+  readonly VITE_GA_KEY: string;
 }
 
 interface ImportMeta {
