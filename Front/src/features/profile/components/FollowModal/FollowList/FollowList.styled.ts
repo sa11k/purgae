23e609cfaf9cc15fa5b91c37cd1f16a9b7f4501a
sa11k <sdk4590@gmail.com>
2022-10-04 14:13:03 +0900

    #423 Feat.스크롤바 파이어폭스 css-KJY

--- Front/src/features/profile/components/FollowModal/FollowList/FollowList.styled.ts
+++ Front/src/features/profile/components/FollowModal/FollowList/FollowList.styled.ts
@@ -3,6 +3,15 @@ import { styled } from "@/styles/theme";
 export const ListDiv = styled.div`
   overflow-y: auto;
   width: 90%;
+  /* &scrollbar {
+    width: 0.3rem;
+    border-radius: 6px;
+    background: rgba(255, 255, 255, 0.4);
+  }
+  &scrollbar-thumb {
+    background: rgba(0, 0, 0, 0.3);
+    border-radius: 6px;
+  } */
   &::-webkit-scrollbar {
     width: 0.3rem;
     border-radius: 6px;
@@ -21,4 +30,5 @@ export const Div = styled.div`
 export const NoFollow = styled.div`
   margin-top: 1rem;
   text-align: center;
+  /* overflow-y: hidden; */
 `;
